399ceeefc0b696d2d16ea31c1d3add2e49b4b5e8
Wiedy Mi <42713027+wiedymi@users.noreply.github.com>
2026-04-06 02:13:54 +0800

    Extract pull request list presentation

--- aizen/Features/Worktree/UI/Components/Git/PullRequests/PullRequestListPane.swift
+++ aizen/Features/Worktree/UI/Components/Git/PullRequests/PullRequestListPane.swift
@@ -15,26 +15,7 @@ struct PullRequestListPane: View {
         static let listBottomPadding: CGFloat = 8
     }
 
-    @Environment(\.controlActiveState) private var controlActiveState
+    @Environment(\.controlActiveState) var controlActiveState
     @ObservedObject var viewModel: PullRequestsViewModel
     @State var hoveredPullRequestID: Int?
-
-    var selectedForegroundColor: Color {
-        controlActiveState == .key ? .accentColor : .accentColor.opacity(0.78)
-    }
-
-    var selectionFillColor: Color {
-        let base = NSColor.unemphasizedSelectedContentBackgroundColor
-        let alpha: Double = controlActiveState == .key ? 0.26 : 0.18
-        return Color(nsColor: base).opacity(alpha)
-    }
-
-    var body: some View {
-        VStack(spacing: 0) {
-            header
-            GitWindowDivider()
-            listContent
-        }
-    }
-
 }
